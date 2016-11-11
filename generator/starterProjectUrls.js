f49bc4e00649c8224f98d1bdc969821de9224d83
Charles Crete <cretezy@gmail.com>
2016-11-11 14:18:14 -0500

    Add react-redux-router-firebase

--- generator/starterProjectUrls.js
+++ generator/starterProjectUrls.js
@@ -109,6 +109,7 @@ module.exports = [
   'https://github.com/wwsun/starter-node-react',
   'https://github.com/xpepermint/isomorphic-react-relay-boilerplate',
   'https://github.com/zxbodya/reactive-widgets',
+  'https://github.com/Cretezy/react-redux-router-firebase',
 
   // package.json elsewhere, or needs tag adjustments
   {
