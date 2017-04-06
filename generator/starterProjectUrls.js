7d1f267b1d16220789fb9d8bf56c8dc70667503a
Iosif Miclaus <iosif.miclaus@gmail.com>
2017-04-06 11:16:54 +0200

    Add react-redux-app

--- generator/starterProjectUrls.js
+++ generator/starterProjectUrls.js
@@ -71,6 +71,7 @@ module.exports = [
   'https://github.com/kriasoft/react-starter-kit',
   'https://github.com/kriasoft/react-static-boilerplate',
   'https://github.com/KyleAMathews/coffee-react-quickstart',
+  'https://github.com/LEINWAND/react-redux-app',
   'https://github.com/lubien/koa-react-redux-universal-boilerplate',
   'https://github.com/lynnaloo/mullet',
   'https://github.com/MadeInHaus/react-flux-gulp-starter',
