125a0c1b18e53c1ed46fbb135b2a3215163f14ac
Rahul Patil <3764685+Technologeek@users.noreply.github.com>
2017-08-05 00:37:06 +0530

    Added my react-webpack3  boiler-plate

--- generator/starterProjectUrls.js
+++ generator/starterProjectUrls.js
@@ -122,6 +122,7 @@ module.exports = [
   'https://github.com/Stupidism/stupid-rc-starter',
   'https://github.com/substack/react-starter-hmr',
   'https://github.com/survivejs/react-component-boilerplate',
+  'https://github.com/Technologeek/react-webpack-3-boilerplate'
   'https://github.com/tinkertrain/jspm-react',
   'https://github.com/tosFa/react-cross-platform-boardgame-example',
   'https://github.com/tpai/r2-starter-kit',
