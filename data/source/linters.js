7584a566466e1849b1ccb56394439afd4050e4e5
Andrew Farmer <ahfarmer@gmail.com>
2017-11-03 16:42:27 -0700

    linter tags

--- data/source/linters.js
+++ data/source/linters.js
@@ -1,6 +1,6 @@
 module.exports = {
-  key: "email-tools",
-  name: "Email Tools",
+  key: "linters",
+  name: "Linters",
   description:
     "Linters find potential errors and/or formatting inconsistencies in your code. Some will automatically fix your code for you.",
   aka: ["code formatters", "beautifiers", "prettifiers"],
@@ -10,16 +10,43 @@ module.exports = {
       name: "html",
       description: "Formats/beautifies HTML.",
     },
+    {
+      name: "css",
+      description: "Formats/beautifies CSS.",
+    },
+    {
+      name: "js",
+      description: "Formats/beautifies JavaScript.",
+    },
+    {
+      name: "opinionated",
+      description:
+        "These don't need configuration because they take a stance on every rule.",
+    },
+    {
+      name: "english",
+      description: "These lint english - not a programming language.",
+    },
   ],
 
   projects: [
-    { url: "https://github.com/beautify-web/js-beautify", tags: ["html"] },
-    { url: "https://github.com/prettier/prettier" },
-    { url: "https://github.com/CSSLint/csslint" },
-    { url: "https://github.com/csscomb/csscomb.js" },
-    { url: "https://github.com/standard/standard" },
-    { url: "https://github.com/douglascrockford/JSLint" },
-    { url: "https://github.com/eslint/eslint" },
-    { url: "https://github.com/jshint/jshint" },
+    {
+      url: "https://github.com/beautify-web/js-beautify",
+      tags: ["js", "html"],
+    },
+    { url: "https://github.com/prettier/prettier", tags: ["js", "css"] },
+    { url: "https://github.com/CSSLint/csslint", tags: ["css"] },
+    { url: "https://github.com/csscomb/csscomb.js", tags: ["css"] },
+    {
+      url: "https://github.com/standard/standard",
+      tags: ["js", "opinionated"],
+    },
+    { url: "https://github.com/douglascrockford/JSLint", tags: ["js"] },
+    { url: "https://github.com/eslint/eslint", tags: ["js"] },
+    { url: "https://github.com/jshint/jshint", tags: ["js"] },
+    { url: "https://github.com/stylelint/stylelint", tags: ["css"] },
+    { url: "https://github.com/btford/write-good", tags: ["english"] },
+    { url: "https://github.com/sindresorhus/xo", tags: ["opinionated"] },
+    { url: "https://github.com/wooorm/alex", tags: ["english"] },
   ],
 };
