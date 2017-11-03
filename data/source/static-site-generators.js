a392fa3d33843e36ebfc8f891d8cbce989f4ea3a
Andrew Farmer <ahfarmer@gmail.com>
2017-11-03 11:56:52 -0700

    fill out static site generators list

--- data/source/static-site-generators.js
+++ data/source/static-site-generators.js
@@ -1,13 +1,38 @@
 module.exports = {
   key: "static-site-generators",
   name: "Static Site Generators",
-  description: "",
-  aka: [""],
+  description: "Tools that help you generate a static site.",
+  aka: ["static site builder", "static site framework"],
 
-  tags: [],
+  tags: [
+    {
+      name: "react",
+      description:
+        "The generator either uses React internally or encourages and supports you using it in combination with the generator."
+    }
+  ],
 
   projects: [
-    { url: "https://github.com/antwarjs/antwar" },
-    { url: "https://github.com/gatsbyjs/gatsby" }
+    { url: "https://github.com/antwarjs/antwar", tags: ["react"] },
+    { url: "https://github.com/gatsbyjs/gatsby", tags: ["react"] },
+    { url: "https://github.com/nozzle/react-static", tags: ["react"] },
+    { url: "https://github.com/jnordberg/wintersmith" },
+    { url: "https://github.com/static-dev/spike" },
+    { url: "https://github.com/phenomic/phenomic" },
+    { url: "https://github.com/segmentio/metalsmith" },
+    { url: "https://github.com/sintaxi/harp" },
+    { url: "https://github.com/assemble/assemble" },
+    { url: "https://github.com/docpad/docpad" },
+    { url: "https://github.com/hexojs/hexo" },
+    { url: "https://github.com/lord/slate", tags: ["documentation"] },
+    {
+      url:
+        "https://github.com/markdalgleish/static-site-generator-webpack-plugin"
+    },
+    { url: "https://github.com/GitbookIO/gitbook" },
+    { url: "https://github.com/elmasse/nextein", tags: ["react"] },
+    { url: "https://github.com/flatiron/blacksmith" },
+    { url: "https://github.com/eduardoboucas/staticman" },
+    { url: "https://github.com/vigetlabs/blendid" }
   ]
 };
