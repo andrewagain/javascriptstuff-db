a67d3855c2cd58fbd3d47e1ac83d9d36fd29d7c0
Andrew Farmer <ahfarmer@gmail.com>
2017-11-04 15:29:05 -0700

    linter autofix tag

--- data/source/linters.js
+++ data/source/linters.js
@@ -27,26 +27,43 @@ module.exports = {
       name: "english",
       description: "These lint english - not a programming language.",
     },
+    {
+      name: "autofix",
+      description: "These linters will automatically fix your code for you.",
+    },
+    {
+      name: "eslint",
+      description: "These linters are powered by ESLint",
+    },
   ],
 
   projects: [
     {
       url: "https://github.com/beautify-web/js-beautify",
-      tags: ["js", "html"],
+      tags: ["js", "html", "autofix"],
     },
-    { url: "https://github.com/prettier/prettier", tags: ["js", "css"] },
+    {
+      url: "https://github.com/prettier/prettier",
+      tags: ["js", "css", "autofix"],
+    },
     { url: "https://github.com/CSSLint/csslint", tags: ["css"] },
     { url: "https://github.com/csscomb/csscomb.js", tags: ["css"] },
     {
       url: "https://github.com/standard/standard",
-      tags: ["js", "opinionated"],
+      tags: ["js", "opinionated", "eslint", "autofix"],
     },
     { url: "https://github.com/douglascrockford/JSLint", tags: ["js"] },
-    { url: "https://github.com/eslint/eslint", tags: ["js"] },
+    {
+      url: "https://github.com/eslint/eslint",
+      tags: ["js", "eslint", "autofix"],
+    },
     { url: "https://github.com/jshint/jshint", tags: ["js"] },
     { url: "https://github.com/stylelint/stylelint", tags: ["css"] },
     { url: "https://github.com/btford/write-good", tags: ["english"] },
-    { url: "https://github.com/sindresorhus/xo", tags: ["opinionated"] },
+    {
+      url: "https://github.com/sindresorhus/xo",
+      tags: ["js", "opinionated", "eslint", "autofix"],
+    },
     { url: "https://github.com/wooorm/alex", tags: ["english"] },
   ],
 };
