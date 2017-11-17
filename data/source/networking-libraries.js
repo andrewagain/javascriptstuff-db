20a22a56e6e94846e9bd6a1c4009d5f7d83eae00
Andrew Farmer <ahfarmer@gmail.com>
2017-11-16 21:55:03 -0800

    more networking libraries

--- data/source/networking-libraries.js
+++ data/source/networking-libraries.js
@@ -4,5 +4,18 @@ module.exports = {
   description: "Tool that assist with making networking or AJAX calls",
   aka: ["AJAX libraries"],
   tags: [],
-  projects: [{ url: "https://github.com/pagekit/vue-resource", tags: ["vue"] }],
+  projects: [
+    { url: "https://github.com/pagekit/vue-resource", tags: ["vue"] },
+    { url: "https://github.com/axios/axios", tags: ["promises"] },
+    { url: "https://github.com/request/request" },
+    { url: "https://github.com/visionmedia/superagent", tags: ["node"] },
+    { url: "https://github.com/developit/unfetch", tags: ["fetch"] },
+    { url: "https://github.com/github/fetch", tags: ["fetch"] },
+    {
+      url: "https://github.com/matthew-andrews/isomorphic-fetch",
+      tags: ["fetch", "node"],
+    },
+    { url: "https://github.com/bitinn/node-fetch", tags: ["fetch", "node"] },
+    { url: "https://github.com/facebook/relay", tags: ["react"] },
+  ],
 };
