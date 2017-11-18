b00ba18cea8ee249d27ba282e3c9b3d07a5d4c40
Andrew Farmer <ahfarmer@gmail.com>
2017-11-18 15:53:08 -0800

    fix bug in starter project automatic tags

--- data/source/react-starter-projects.js
+++ data/source/react-starter-projects.js
@@ -1,3 +1,5 @@
+const starterProjectTags = require("./react-starter-project-tags");
+
 module.exports = {
   key: "react-starter-projects",
   name: "React Starter Projects",
@@ -5,105 +7,7 @@ module.exports = {
   description:
     "Repositories that you can copy and modify to create your own React app.",
   aka: ["react boilerplates", "react starters", "react seeds"],
-  tags: [
-    {
-      name: "dependency",
-      description:
-        "Rather than copying the project, you add it as a dependency.",
-    },
-    {
-      name: "gatsby",
-      description:
-        "Gatsby is a static site generator. These starters are static sites built with Gatsby.",
-    },
-    { name: "babel", description: "Babel transpiles JavaScript." },
-    {
-      name: "coffeescript",
-      aka: ["coffee", "coffee script"],
-      description: "CoffeeScript is a language that compiles into JavaScript",
-    },
-    {
-      name: "css modules",
-      aka: ["css-modules"],
-      description:
-        "A CSS Module is a CSS file in which all class names and animation names are scoped locally by default.",
-    },
-    {
-      name: "es6",
-      aka: ["esnext", "es6+", "es7", "es2015", "es2016", "transpiled"],
-      description:
-        "Uses transpiling to convert JavaScript with the latest language features into well-supported (ES5) code",
-    },
-    {
-      name: "flux",
-      description:
-        "Something flux-like is used: Redux, Mobx, Flux, Reflux, etc.",
-    },
-    {
-      name: "hmr",
-      aka: ["hot", "hot module reloading"],
-      description:
-        "Supports updating JavaScript in the browser without a refresh.",
-    },
-    {
-      name: "inline style",
-      aka: ["inline CSS"],
-      description:
-        "CSS is added with inline style properties rather than in a separate stylesheet.",
-    },
-    {
-      name: "linter",
-      aka: ["lint"],
-      description:
-        "Linters are used to catch errors and coding style issues early.",
-    },
-    {
-      name: "live reload",
-      aka: ["auto reload", "browser reload"],
-      description:
-        "Automatic browser refresh whenever the developer makes code changes in the text editor.",
-    },
-    {
-      name: "minimal",
-      aka: ["simple"],
-      description:
-        "A project is considered 'minimal' if it has fewer than 20 dependencies.",
-    },
-    { name: "rails" },
-    { name: "native" },
-    {
-      name: "router",
-      aka: ["routing"],
-      description: "Uses react-router to keep the UI in sync with the URL.",
-    },
-    {
-      name: "tests",
-      aka: ["test", "unit test"],
-      description: "The project has same framework set up for testing.",
-    },
-    {
-      name: "typescript",
-      description:
-        "TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.",
-    },
-    {
-      name: "universal",
-      aka: ["isomorphic"],
-      description:
-        "Runs on both the client and the server, usually with Express or Koa.",
-    },
-    {
-      name: "webpack 2",
-      aka: ["webpack2"],
-      description:
-        "Webpack 2 was released in Jan 2017 and includes some breaking changes.",
-    },
-    {
-      name: "flow",
-      aka: "flowtype",
-      description: "Static type checker by Facebook",
-    },
-  ],
+  tags: starterProjectTags,
   projects: [
     {
       url: "https://github.com/gatsbyjs/gatsby",
