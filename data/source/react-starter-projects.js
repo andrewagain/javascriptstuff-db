73c27e8796d84398d63d6b24bf90450ac598c6fc
ed <ed.proff@gmail.com>
2017-11-18 19:07:13 +0300

    add lnked/react-starter

--- data/source/react-starter-projects.js
+++ data/source/react-starter-projects.js
@@ -423,5 +423,10 @@ module.exports = {
     { url: "https://github.com/StephenGrider/ReduxSimpleStarter" },
     { url: "https://github.com/alinz/example-react-native-redux" },
     { url: "https://github.com/mirrorjs/mirror", tags: ["dependency"] },
+    {
+      url: "https://github.com/lnked/react-starter",
+      tags: ["hmr", "PWA", "postcss", "babel", "css modules", "webpack3", "stylelint", "babel", "es6"],
+      packagePaths: ["package.json"],
+    },
   ],
 };
